97f1634b248f32ba8be69f5a9327b1c11bd89635
burui <burui@integritytech.com.cn>
2023-08-02 15:13:26 +0800

    fix info

--- Script/caiyun.js
+++ Script/caiyun.js
@@ -37,9 +37,6 @@ if ($request.url.indexOf('user') != -1){
     body.result.wt.svip_given = 9999;
     body.result.wt.ranking_above = 99;
     body.result.is_phone_verified = true;
-    body.result.name = "叮当猫の分享频道";
-    body.result.avatar = "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLia6zPUuGQKVOJk2gnjpjHxSIuH7XaOJF2gEk9ic35ibib4QzUcbvNu6EpdHDc1Vciat1xg63ibK6EptWw/132";
-    body.result.phone_num = "13145200000";
     body.result.vip_take_effect = 1;
     body.result.is_primary = true;
     body.result.xy_vip_expire = 4092599349;
@@ -47,13 +44,6 @@ if ($request.url.indexOf('user') != -1){
     body.result.svip_take_effect = 1;
     body.result.vip_type = "s";
     body.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
-    body.result.bound_status.qq.username = "https://t.me/chxm1023";
-    body.result.bound_status.weixin.username = "https://t.me/chxm1023";
-    body.result.bound_status.caiyun.username = "https://t.me/chxm1023";
-    body.result.bound_status.twitter.username = "https://t.me/chxm1023";
-    body.result.bound_status.facebook.username = "https://t.me/chxm1023";
-    body.result.bound_status.apple.username = "https://t.me/chxm1023";
-    body.result.bound_status.weibo.username = "https://t.me/chxm1023";
 }
 
 $done({ body: JSON.stringify(body)});
